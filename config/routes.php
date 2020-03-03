872851e136ae50fa1b541f838cdbb05d9786717f
hant1607 <nguyenha98nq@gmail.com>
2020-03-03 11:42:47 +0700

    edit route

--- config/routes.php
+++ config/routes.php
@@ -22,6 +22,7 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
+
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
